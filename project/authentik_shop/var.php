14a64bff32413453ebeba11c4d1029cfa2f3ec48
lucieleclaire <lucie.leclaire44@gmail.com>
2021-08-25 14:03:37 -0400

    Portfolio

---
+++ project/authentik_shop/var.php
@@ -0,0 +1,65 @@
+<?php 
+        
+    $products = [];
+    
+    $products[] = [
+        'name' => 'French Bulldog - Free Hugs',
+        'price' => '29.97$',
+        'productImage' => 'img/product1.png', 
+        'link' => 'https://teespring.com/fr/new-french-bulldog-free-hugs?pid=2&cid=2397'
+    ];
+
+    $products[] = [
+        'name' => 'Love',
+        'price' => '46.44$',
+        'productImage' => 'img/product2.png', 
+        'link' => 'https://teespring.com/fr/t-shirt-love-8555?pid=823&cid=103662'
+    ];
+    
+    $products[] = [
+        'name' => 'Tattooed Chihuahua',
+        'price' => '46.44$',
+        'productImage' => 'img/product3.png', 
+        'link' => 'https://teespring.com/fr/tattooed-chihuahua-puppy-t-shi?pid=823&cid=103662'
+    ];
+
+        $products[] = [
+        'name' => 'Tiger Lover',
+        'price' => '30.05$',
+        'productImage' => 'img/product4.png', 
+        'link' => 'https://teespring.com/fr/tiger-lover-june-2020?pid=87&cid=2325'
+    ];
+
+        $products[] = [
+        'name' => 'Tiger',
+        'price' => '46.32$',
+        'productImage' => 'img/product5.png', 
+        'link' => 'https://teespring.com/fr/t-shirt-tiger-3488?pid=823&cid=103575'
+    ];
+
+        $products[] = [
+        'name' => 'Tiger Pillow',
+        'price' => '34.69$',
+        'productImage' => 'img/product6.png', 
+        'link' => 'https://teespring.com/fr/tiger-pillow-4411?pid=585&cid=102398'
+    ];
+
+    $teams = [];
+    
+    $teams[] = [
+        'name' => 'Taylor Swift',
+        'age' => '25 years',
+        'teamImage' => 'img/team1.jpg', 
+    ];
+        $teams[] = [
+        'name' => 'John Mayer',
+        'age' => '28 years',
+        'teamImage' => 'img/team2.jpg', 
+    ];
+        $teams[] = [
+        'name' => 'Steve Jobs',
+        'age' => '23 years',
+        'teamImage' => 'img/team3.jpg', 
+    ];
+
+?>
---
+++ project/tattoo_shop/js/script.js
@@ -0,0 +1,5 @@
+        AOS.init({
+            delay: 20, // values from 0 to 3000, with step 50ms
+            duration: 2000,
+
+        });
